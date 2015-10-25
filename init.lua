7325ebba2c12158631e35512a87546020bf9b39b
wilkgr76 <wilk.gr76@gmail.com>
2015-10-25 10:45:38 +1000

    Added steel bars (from xpanes, from minetest_game)
    xpanes uses steel ingots to craft iron bars, the iron bars have been renamed to steel bars.

--- init.lua
+++ init.lua
@@ -131,3 +131,28 @@ minetest.register_craftitem(":default:diamond", {
 	inventory_image = "default_diamond.png",
 	wield_scale = {x=1.5, y=1.5, z=1},
 })
+
+-- Steel bars
+
+xpanes.register_pane("bar", {
+	description = "Steel bar",
+	tiles = {"xpanes_space.png"},
+	drawtype = "airlike",
+	paramtype = "light",
+	is_ground_content = false,
+	sunlight_propagates = true,
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	air_equivalent = true,
+	textures = {"xpanes_bar.png","xpanes_bar.png","xpanes_space.png"},
+	inventory_image = "xpanes_bar.png",
+	wield_image = "xpanes_bar.png",
+	groups = {snappy=2, cracky=3, oddly_breakable_by_hand=3, pane=1},
+	sounds = default.node_sound_stone_defaults(),
+	recipe = {
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:steel_ingot'}
+	}
+})
