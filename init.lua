97e04b370bf49af66aad77c576652e0b4402a3fe
wilkgr76 <wilk.gr76@gmail.com>
2015-10-25 11:10:38 +1000

    Bug fix (crashed before)

--- init.lua
+++ init.lua
@@ -157,43 +157,43 @@ xpanes.register_pane("bar", {
 	}
 })
 
-minetest.register_craftitem("default:steel_ingot", {
+minetest.register_craftitem(":default:steel_ingot", {
 	description = "Steel Ingot",
 	inventory_image = "default_steel_ingot.png",
 	wield_scale = {x=1.5, y=1.5, z=1},
 })
 
-minetest.register_craftitem("default:copper_ingot", {
+minetest.register_craftitem(":default:copper_ingot", {
 	description = "Copper Ingot",
 	inventory_image = "default_copper_ingot.png",
 	wield_scale = {x=1.5, y=1.5, z=1},
 })
 
-minetest.register_craftitem("default:bronze_ingot", {
+minetest.register_craftitem(":default:bronze_ingot", {
 	description = "Bronze Ingot",
 	inventory_image = "default_bronze_ingot.png",
 	wield_scale = {x=1.5, y=1.5, z=1},
 })
 
-minetest.register_craftitem("default:gold_ingot", {
+minetest.register_craftitem(":default:gold_ingot", {
 	description = "Gold Ingot",
 	inventory_image = "default_gold_ingot.png",
 	wield_scale = {x=1.5, y=1.5, z=1},
 })
 
-minetest.register_craftitem("default:mese_crystal_fragment", {
+minetest.register_craftitem(":default:mese_crystal_fragment", {
 	description = "Mese Crystal Fragment",
 	inventory_image = "default_mese_crystal_fragment.png",
 	wield_scale = {x=1.5, y=1.5, z=1},
 })
 
-minetest.register_craftitem("default:clay_brick", {
+minetest.register_craftitem(":default:clay_brick", {
 	description = "Clay Brick",
 	inventory_image = "default_clay_brick.png",
 	wield_scale = {x=1.5, y=1.5, z=1},
 })
 
-minetest.register_craftitem("default:obsidian_shard", {
+minetest.register_craftitem(":default:obsidian_shard", {
 	description = "Obsidian Shard",
 	inventory_image = "default_obsidian_shard.png",
 	wield_scale = {x=1.5, y=1.5, z=1},
