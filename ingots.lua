3a7c30124824aabd79ce775509cf6c5dc39ff3a8
wilkgr <wilkgr76@users.noreply.github.com>
2015-10-27 11:51:43 +1000

    Added content.

--- ingots.lua
+++ ingots.lua
@@ -1,1 +1,41 @@
+minetest.register_craftitem(":default:steel_ingot", {
+	description = "Steel Ingot",
+	inventory_image = "default_steel_ingot.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
 
+minetest.register_craftitem(":default:copper_ingot", {
+	description = "Copper Ingot",
+	inventory_image = "default_copper_ingot.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:bronze_ingot", {
+	description = "Bronze Ingot",
+	inventory_image = "default_bronze_ingot.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:gold_ingot", {
+	description = "Gold Ingot",
+	inventory_image = "default_gold_ingot.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:mese_crystal_fragment", {
+	description = "Mese Crystal Fragment",
+	inventory_image = "default_mese_crystal_fragment.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:clay_brick", {
+	description = "Clay Brick",
+	inventory_image = "default_clay_brick.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:obsidian_shard", {
+	description = "Obsidian Shard",
+	inventory_image = "default_obsidian_shard.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
