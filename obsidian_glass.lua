25aedba1f8f0820ba27f74695f65e773a498a17f
wilkgr <wilkgr76@users.noreply.github.com>
2015-10-27 11:53:29 +1000

    Create obsidian_glass.lua

---
+++ obsidian_glass.lua
@@ -0,0 +1,10 @@
+minetest.register_node(":default:obsidian_glass", {
+	description = "Obsidian Glass",
+	drawtype = "glasslike_framed_optional",
+	tiles = {"default_obsidian_glass.png", "default_obsidian_glass_detail.png"},
+	paramtype = "light",
+	is_ground_content = false,
+	sunlight_propagates = true,
+	sounds = default.node_sound_glass_defaults(),
+	groups = {cracky=3,oddly_breakable_by_hand=3},
+})
