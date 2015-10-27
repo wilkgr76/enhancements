ccbfb8400d787a5a1a9fa3dc69735b5206a6ae01
wilkgr <wilkgr76@users.noreply.github.com>
2015-10-27 11:52:56 +1000

    Create lumps.lua

---
+++ lumps.lua
@@ -0,0 +1,43 @@
+minetest.register_craftitem(":default:coal_lump", {
+	description = "Coal Lump",
+	inventory_image = "default_coal_lump.png",
+	groups = {coal = 1},
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+
+minetest.register_craftitem(":default:clay_lump", {
+	description = "Clay Lump",
+	inventory_image = "default_clay_lump.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:iron_lump", {
+	description = "Iron Lump",
+	inventory_image = "default_iron_lump.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:copper_lump", {
+	description = "Copper Lump",
+	inventory_image = "default_copper_lump.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:mese_crystal", {
+	description = "Mese Crystal",
+	inventory_image = "default_mese_crystal.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:gold_lump", {
+	description = "Gold Lump",
+	inventory_image = "default_gold_lump.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
+
+minetest.register_craftitem(":default:diamond", {
+	description = "Diamond",
+	inventory_image = "default_diamond.png",
+	wield_scale = {x=1.5, y=1.5, z=1},
+})
