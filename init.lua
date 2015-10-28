c55c22014be05068256edcf0a39e4d3f5195962f
NathanSalapat <rendertracer@gmail.com>
2015-10-28 10:29:32 -0500

    fixed init.lua error.

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
-dofile(minetest.get_modpath("enhancements").."/swords.lua"), -- Changes swords
-dofile(minetest.get_modpath("enhancements").."/lumps.lua"), -- Changes lumps, diamonds
-dofile(minetest.get_modpath("enhancements").."/ingots.lua"), -- Changes ingots
-dofile(minetest.get_modpath("enhancements").."/obsidian_glass.lua"), --Makes obsidian glass joined
-dofile(minetest.get_modpath("enhancements").."/steel_bars.lua"), -- Renames iron bars to steel bars
+dofile(minetest.get_modpath("enhancements").."/swords.lua") -- Changes swords
+dofile(minetest.get_modpath("enhancements").."/lumps.lua") -- Changes lumps, diamonds
+dofile(minetest.get_modpath("enhancements").."/ingots.lua") -- Changes ingots
+dofile(minetest.get_modpath("enhancements").."/obsidian_glass.lua") --Makes obsidian glass joined
+dofile(minetest.get_modpath("enhancements").."/steel_bars.lua") -- Renames iron bars to steel bars
