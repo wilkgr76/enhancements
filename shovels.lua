77c494fd616a1bbd87fef6d0144717ca36fa73b5
wilkgr <wilkgr76@users.noreply.github.com>
2015-10-30 08:37:28 +1000

    Changes shovels so that they are no longer in the way.

---
+++ shovels.lua
@@ -0,0 +1,78 @@
+minetest.register_tool(":default:shovel_wood", {
+	description = "Wooden Shovel",
+	inventory_image = "default_tool_woodshovel.png",
+	wield_image = "default_tool_woodshovel.png",
+	tool_capabilities = {
+		full_punch_interval = 1.2,
+		max_drop_level=0,
+		groupcaps={
+			crumbly = {times={[1]=3.00, [2]=1.60, [3]=0.60}, uses=10, maxlevel=1},
+		},
+		damage_groups = {fleshy=2},
+	},
+})
+minetest.register_tool(":default:shovel_stone", {
+	description = "Stone Shovel",
+	inventory_image = "default_tool_stoneshovel.png",
+	wield_image = "default_tool_stoneshovel.png",
+	tool_capabilities = {
+		full_punch_interval = 1.4,
+		max_drop_level=0,
+		groupcaps={
+			crumbly = {times={[1]=1.80, [2]=1.20, [3]=0.50}, uses=20, maxlevel=1},
+		},
+		damage_groups = {fleshy=2},
+	},
+})
+minetest.register_tool(":default:shovel_steel", {
+	description = "Steel Shovel",
+	inventory_image = "default_tool_steelshovel.png",
+	wield_image = "default_tool_steelshovel.png",
+	tool_capabilities = {
+		full_punch_interval = 1.1,
+		max_drop_level=1,
+		groupcaps={
+			crumbly = {times={[1]=1.50, [2]=0.90, [3]=0.40}, uses=30, maxlevel=2},
+		},
+		damage_groups = {fleshy=3},
+	},
+})
+minetest.register_tool(":default:shovel_bronze", {
+	description = "Bronze Shovel",
+	inventory_image = "default_tool_bronzeshovel.png",
+	wield_image = "default_tool_bronzeshovel.png",
+	tool_capabilities = {
+		full_punch_interval = 1.1,
+		max_drop_level=1,
+		groupcaps={
+			crumbly = {times={[1]=1.50, [2]=0.90, [3]=0.40}, uses=40, maxlevel=2},
+		},
+		damage_groups = {fleshy=3},
+	},
+})
+minetest.register_tool(":default:shovel_mese", {
+	description = "Mese Shovel",
+	inventory_image = "default_tool_meseshovel.png",
+	wield_image = "default_tool_meseshovel.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=3,
+		groupcaps={
+			crumbly = {times={[1]=1.20, [2]=0.60, [3]=0.30}, uses=20, maxlevel=3},
+		},
+		damage_groups = {fleshy=4},
+	},
+})
+minetest.register_tool(":default:shovel_diamond", {
+	description = "Diamond Shovel",
+	inventory_image = "default_tool_diamondshovel.png",
+	wield_image = "default_tool_diamondshovel.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			crumbly = {times={[1]=1.10, [2]=0.50, [3]=0.30}, uses=30, maxlevel=3},
+		},
+		damage_groups = {fleshy=4},
+	},
+})
